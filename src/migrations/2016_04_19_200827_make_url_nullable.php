28b7f65553b2f1a932377513b53d16eff76c528d
Tom Witkowski <dev.gummibeer@gmail.com>
2016-04-19 20:19:18 +0200

    #84 make url nullable and don't require it

--- src/migrations/2016_04_19_200827_make_url_nullable.php
+++ src/migrations/2016_04_19_200827_make_url_nullable.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationsTable extends Migration
+class MakeUrlNullable extends Migration
 {
 
     /**
