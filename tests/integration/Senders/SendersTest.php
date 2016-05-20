78312c64d68e8904c1a1fd989d04ee4ce1b693aa
Tom Witkowski <dev.gummibeer@gmail.com>
2016-05-20 14:44:28 +0200

    fix pluck in older laravel versions

--- tests/integration/Senders/SendersTest.php
+++ tests/integration/Senders/SendersTest.php
@@ -61,7 +61,7 @@ class SendersTest extends TestCaseDB
         $this->senders->sendNow($singleNotification);
 
         $notifications = Notification::all();
-        $stackIds = $notifications->pluck('stack_id')->unique()->toArray();
+        $stackIds = array_unique($notifications->lists('stack_id'));
 
         $this->assertCount(1, $notifications);
         $this->assertCount(1, $stackIds);
@@ -90,7 +90,7 @@ class SendersTest extends TestCaseDB
         $this->senders->sendNow($sendMultiple);
 
         $notifications = Notification::all();
-        $stackIds = $notifications->pluck('stack_id')->unique()->toArray();
+        $stackIds = array_unique($notifications->lists('stack_id'));
 
         $this->assertCount(2, $notifications);
         $this->assertCount(1, $stackIds);
